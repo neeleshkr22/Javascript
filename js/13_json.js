709ba14998f5bf103507fc3c84699dccd4ac4beb
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2023-11-21 19:28:25 +0530

    Update 13_json.js

--- js/13_json.js
+++ js/13_json.js
@@ -1,1 +1,83 @@
 //Json is basically used for APIs and act/used as a object like structure
+
+
+/*{
+    //json
+    name: "neelesh",
+    "coursename": "Javascript",
+}*/
+
+
+//array format
+[
+    {},
+    {},
+
+]
+//random user me api
+/*{
+    "results": [
+      {
+        "gender": "female",
+        "name": {
+          "title": "Miss",
+          "first": "Sophie",
+          "last": "Jones"
+        },
+        "location": {
+          "street": {
+            "number": 9614,
+            "name": "King St"
+          },
+          "city": "Delta",
+          "state": "Nova Scotia",
+          "country": "Canada",
+          "postcode": "D1W 8W1",
+          "coordinates": {
+            "latitude": "-64.9304",
+            "longitude": "131.4053"
+          },
+          "timezone": {
+            "offset": "-6:00",
+            "description": "Central Time (US & Canada), Mexico City"
+          }
+        },
+        "email": "sophie.jones@example.com",
+        "login": {
+          "uuid": "f753f848-21f1-44e4-a425-0b4f09ace692",
+          "username": "goldenbear764",
+          "password": "bp2002",
+          "salt": "f7fs6r4s",
+          "md5": "45bed68f8cdcac2ac99caa3591fbc087",
+          "sha1": "e1dbc894fc1d32e75207bc281f6bf2be747dd4a6",
+          "sha256": "6f513b46e25f03c6f5292f42a19dd4509afdfb6f1d8df7ed5e21fae05181007b"
+        },
+        "dob": {
+          "date": "1959-06-14T07:11:04.904Z",
+          "age": 64
+        },
+        "registered": {
+          "date": "2021-09-06T05:17:42.628Z",
+          "age": 2
+        },
+        "phone": "P67 X36-1709",
+        "cell": "Z86 Y80-7278",
+        "id": {
+          "name": "SIN",
+          "value": "918114281"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/women/1.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/women/1.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/women/1.jpg"
+        },
+        "nat": "CA"
+      }
+    ],
+    "info": {
+      "seed": "e64ba68656f8b299",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }*/
