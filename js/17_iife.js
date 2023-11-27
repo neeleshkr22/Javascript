6b63fb0b0e0bfb9aa34d22845f96d7670f4c1f39
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2023-11-27 23:04:20 +0530

    Update 17_iife.js

--- js/17_iife.js
+++ js/17_iife.js
@@ -1,5 +1,6 @@
 //Immediately Invoked Function Expression (IIFE)
 //to prevent global scope we use IIFE
+//An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
 
 (function one(){
     //named IIFE
@@ -11,4 +12,11 @@
     console.log(`DB connected ${name}`);
 })('Neelesh');
 
+(() => {
+  // some initiation code
+  let firstVariable;
+  let secondVariable;
+})();
+
+// firstVariable and secondVariable will be discarded after the function is executed.
 
