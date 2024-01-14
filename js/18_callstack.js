a3b2e568ff0a47ff9628cea32d36097b2f93bd2e
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-01-14 22:40:07 +0530

    Update 18_callstack.js

--- js/18_callstack.js
+++ js/18_callstack.js
@@ -1,3 +1,18 @@
 //execution context
-//calls(call stack)
+//calls(call stack) how function execute in memory
 //global execution context
+//Javascript execution context divides programmes in two phase refers it to (this). Global execution context of node js is diffrent, web is diffrent, bun is diffrent and execute in thread
+//context are of types:
+/*Global Execution Context 
+Function execution Context
+Eval Executin Context
+
+Javascript run in two phase:
+Memory creation phase(or called creation phase): In this phase only memory is allocated to variables function 
+Execution phase: Code is executed in this phase
+ For everytime the code is executed, a context is created, which consist of:
+ New Variable environment
+  execution thread
+It returns the variable to parent context ie. global execution context, and then it is delete to free up the memory
+
+*/
