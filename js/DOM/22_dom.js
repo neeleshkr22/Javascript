9e54d1bca8ed1fa0287006649dd5beb8fd4c4ab5
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-01-25 23:21:55 +0530

    Update 22_dom.js

--- js/DOM/22_dom.js
+++ js/DOM/22_dom.js
@@ -1,3 +1,5 @@
 document.getElementById("title")
 document.getElementById("title").id
 document.getElementById("title").className  //will give class
+const title = document.getElement("title")
+title.style.color = 'cyan'
