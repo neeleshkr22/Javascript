3e71dd64cb473fe0b89b403de96a9c2a36cf1d89
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-02-04 17:16:29 +0530

    Update 22_dom.js

--- js/DOM/22_dom.js
+++ js/DOM/22_dom.js
@@ -3,3 +3,23 @@ document.getElementById("title").id
 document.getElementById("title").className  //will give class
 const title = document.getElement("title")
 title.style.color = 'cyan'
+/*
+selecting with id
+document.getElementById("id-name")
+
+selecting with class
+document.getElementsByClassName("className")
+
+selecting with tag
+document.getElementsByTagName("p")
+
+*/
+
+/*
+Properties of DOM manipulation
+TagName:- return tag for elements nodes
+innerText:- returns the text content of the element and all its children
+innerHTML:- returns the plain text or HTML contents in the element
+teextContent:- returns textual content even for hidden elements
+
+*/
