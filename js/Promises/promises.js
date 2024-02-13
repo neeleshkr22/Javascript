6e9ffd5bfa2002877c5c66c88fc5f332679d770e
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-02-13 22:16:00 +0530

    Update promises.js

--- js/Promises/promises.js
+++ js/Promises/promises.js
@@ -10,5 +10,52 @@ const promiseOne = new Promise(function(resolve, reject){
     //DB calls, cryptography, network call
     setTimeout(function(){
         console.log('Async task is complete');
+        resolve() //will connect to .then
     },1000)
+})
+//.then has a connection with resolve
+promiseOne.then(function(){
+    console.log("Promise consume");
+})
+
+
+//another way of calling .then and resolve
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolve");
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username: "Neelesh", email: "neeleshrana22@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);   //will get all the parameter of resolve
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(()=> {
+        //let error = true;
+        let error = false;
+        if(!error){
+            resolve({username : "Neelesh", password: "1234"})
+        }else{
+            reject('ERROR: Something went wrong')                              //reject will throw error
+        }
+    },1000)
+})
+
+promiseFour.then((user)=> {
+    console.log(user);
+    return user.username;
+}).then((username)=>{                    //chaining 
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
 })
