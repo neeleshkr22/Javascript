5ac0ec6231adc6404d03ff658000f5bf67db4242
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-02-14 21:06:18 +0530

    Update promises.js

--- js/Promises/promises.js
+++ js/Promises/promises.js
@@ -18,7 +18,8 @@ promiseOne.then(function(){
     console.log("Promise consume");
 })
 
-
+//-----------------------------------------------------Two-----------------------------------------------------------------------------
+ 
 //another way of calling .then and resolve
 new Promise(function(resolve,reject){
     setTimeout(function(){
@@ -28,6 +29,8 @@ new Promise(function(resolve,reject){
 }).then(function(){
     console.log("Async 2 resolve");
 })
+
+//-------------------------------------------------------Three---------------------------------------------------------
 
 const promiseThree = new Promise(function(resolve,reject){
     setTimeout(function(){
@@ -39,10 +42,12 @@ promiseThree.then(function(user){
     console.log(user);   //will get all the parameter of resolve
 })
 
+//-----------------------------------------------------Four---------------------------------------------------------------
+
 const promiseFour = new Promise(function(resolve,reject){
     setTimeout(()=> {
-        //let error = true;
-        let error = false;
+        let error = true;
+        //let error = false;
         if(!error){
             resolve({username : "Neelesh", password: "1234"})
         }else{
@@ -58,4 +63,54 @@ promiseFour.then((user)=> {
     console.log(username);
 }).catch(function(error){
     console.log(error);
+}).finally(()=> {
+    console.log("The promise is either reolved or rejected");
 })
+
+//----------------------------------------------------Five---------------------------------------------------------------------
+
+const promiseFive  = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = true;
+        if(!error){
+            resolve({username: "Neelesh" , password : "Javascript"})
+        }else{
+            reject('ERROR: SOMETHING WENT WRONG IN THIS')
+        }
+    },1000)
+})
+
+async function consumepromisefive(){
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumepromisefive();
+
+//------------------------------------------------------------APIs using Async------------------------------------------------------------
+
+async function getAllUsers(){
+    try {
+        const respose = await fetch('https://jsonplaceholder.typicode.com/users');
+        
+        const data = await respose.json()
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+getAllUsers();
+
+
+//------Now .then() and .catch()
+
+fetch('https://api.github.com/users/neeleshkr22')
+.then((response)=>{
+    return response.json()})
+.then((data)=>{
+    console.log(data);})
+.catch((error)=>{
+    console.log(error);})
