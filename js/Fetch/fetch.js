9230cbe6e84735194f8fab4c1a6b256d9ca3fac1
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-02-17 22:41:15 +0530

    Update fetch.js

--- js/Fetch/fetch.js
+++ js/Fetch/fetch.js
@@ -1,1 +1,3 @@
 //The global fetch() method starts the process of fetching a resource from network, returning a promise which is fulfilled once responsse is available. 
+//The fetch() method starts the process of fetching a resource from a server.
+//The fetch() method returns a Promise that resolves to a Response object.
