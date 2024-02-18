514476cce66c3b5f74d56052f2fa34424609c4a1
Neelesh Kumar <130912265+neeleshkr22@users.noreply.github.com>
2024-02-18 18:40:29 +0530

    Update fetch.js

--- js/Fetch/fetch.js
+++ js/Fetch/fetch.js
@@ -1,3 +1,6 @@
 //The global fetch() method starts the process of fetching a resource from network, returning a promise which is fulfilled once responsse is available. 
 //The fetch() method starts the process of fetching a resource from a server.
 //The fetch() method returns a Promise that resolves to a Response object.
+//JS engine contains memory heap and call stack
+//fetch produce microtask queue(priority queue)
+
